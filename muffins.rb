2427da551dfba94c8fa72237741218aeaf4e5de8
Jonathan Lau <jonhlau28@gmail.com>
2019-03-27 21:23:33 -0700

    Init REPL

---
+++ muffins.rb
@@ -0,0 +1,51 @@
+def initial_prompt
+  user = `git config user.name`
+  puts " ___________________"
+  puts "  Hi, #{user}".delete!("\n")
+  puts "  -------------------"
+  puts "   \\"
+  puts "    \\"
+  puts "        .--."
+  puts "       |o_o |"
+  puts "       |:_/ |"
+  puts "      //   \ \\"
+  puts "     (|     | )"
+  puts "    /\"\_   _/`\\"
+  puts "    \\___)=(___/`"
+
+  puts "\n\n"
+end 
+
+class Muffins
+  class << self 
+    def options
+      puts " 
+        1: Start Postgresql
+        2: Stop Postgresql
+        3: Start Rails
+        4: Stop Rails 
+        5: Restart Rails 
+        6: Load Remote data 
+        7: Load seed data
+        8. Exit"
+      end 
+  end 
+end 
+
+muffins = Muffins.new
+
+def handle_input(input)
+  result = eval(input)
+  puts(" => #{result}")
+end
+
+repl = -> prompt do
+  puts "Rails Helper"
+  puts initial_prompt
+  puts Muffins.options
+  handle_input(gets.chomp!)
+end
+
+loop do
+  repl[">> "]
+end
